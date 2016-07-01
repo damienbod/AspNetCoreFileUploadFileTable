da82f929c2b9780c345518f2a7f02b549cf103fd
damienbod <damien_bod@hotmail.com>
2016-07-02 00:33:37 +0200

    Updating to ASP.NET Core RTM

--- src/AspNet5FileUploadFileTable/Controllers/FileUploadController.cs
+++ src/AspNet5FileUploadFileTable/Controllers/FileUploadController.cs
@@ -41,6 +41,9 @@ namespace AspNet5FileUploadFileTable.Controllers
                     {
                         var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
                         contentTypes.Add(file.ContentType);
+
+                        
+
                         names.Add(fileName);
 
                         // Extension method update RC2 has removed this 
