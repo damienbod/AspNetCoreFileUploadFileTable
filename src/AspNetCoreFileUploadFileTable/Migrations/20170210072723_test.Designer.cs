720f236558df5229c89f14ecb58c118f36991348
damienbod <damien_bod@hotmail.com>
2017-02-10 08:29:39 +0100

    renaming the assembly

---
+++ src/AspNetCoreFileUploadFileTable/Migrations/20170210072723_test.Designer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using DataAccess;
+
+namespace AspNetCoreFileUploadFileTable.Migrations
+{
+    [DbContext(typeof(FileContext))]
+    [Migration("20170210072723_test")]
+    partial class test
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("DataAccess.Model.FileDescription", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ContentType");
+
+                    b.Property<DateTime>("CreatedTimestamp");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("FileName");
+
+                    b.Property<DateTime>("UpdatedTimestamp");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("FileDescriptions");
+                });
+        }
+    }
+}
