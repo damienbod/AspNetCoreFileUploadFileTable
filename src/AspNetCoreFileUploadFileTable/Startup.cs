ef1c29981fac8b51159e41b0ccd642d9dbd80dd8
damienbod <damien_bod@hotmail.com>
2019-06-16 10:09:25 +0200

    updating startup

--- src/AspNetCoreFileUploadFileTable/Startup.cs
+++ src/AspNetCoreFileUploadFileTable/Startup.cs
@@ -40,7 +40,6 @@ namespace AspNet5FileUploadFileTable
             services.AddScoped<ValidateMimeMultipartContentFilter>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
