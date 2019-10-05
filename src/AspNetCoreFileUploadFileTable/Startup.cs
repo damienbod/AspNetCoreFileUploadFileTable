31bab2d3572e696e8f55bf53e1bfdd41fbeff5d5
damienbod <damien_bod@hotmail.com>
2019-10-05 10:16:08 +0200

    clean up default MVC route

--- src/AspNetCoreFileUploadFileTable/Startup.cs
+++ src/AspNetCoreFileUploadFileTable/Startup.cs
@@ -60,7 +60,7 @@ namespace AspNetCoreFileUploadFileTable
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=FileClient}/{action=ViewAllFiles}/{id?}");
             });
         }
     }
